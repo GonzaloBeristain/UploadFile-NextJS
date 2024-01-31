002d27856b0b8202e0d02441aa7ff7af13764665
Gonzalo Beristain <ga_beristainn@hotmail.com>
2024-01-31 20:03:10 -0300

    Styles with Tailwind

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -3,18 +3,21 @@ import { writeFile } from "fs/promises";
 import path from "path";
 
 export async function POST(request) {
+    try {
+        const data = await request.formData()
+        const file = data.get("file")
+        
+        const bytes = await file.arrayBuffer()
+        const buffer = Buffer.from(bytes)
 
-    const data = await request.formData()
-    const file = data.get("file")
-    
-    const bytes = await file.arrayBuffer()
-    const buffer = Buffer.from(bytes)
+        const filePath = path.join(process.cwd(), "public", file.name)
+        writeFile(filePath, buffer)
+        console.log("file uploaded to", filePath)
 
-    const filePath = path.join(process.cwd(), "public", file.name)
-    writeFile(filePath, buffer)
-    console.log("file uploaded to", filePath)
-
-    return new NextResponse(JSON.stringify({
-        message: "Uploaded file"
-    }))
+        return new NextResponse(JSON.stringify({
+            message: "Uploaded file"
+        }))
+    } catch (error) {
+            return new NextResponse("Error uploading the file", { status: 500 })
+    }
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,18 @@
 "use client";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function HomePage() {
   const [file, setFile] = useState();
 
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0])
+  };
+
   return (
-    <div>
-      <h1>Upload</h1>
-      
-      <form onSubmit={async (e) => {
+    <div className="flex flex-col justify-center items-center h-[calc(100vh-7rem)]">
+      <section className="bg-slate-900 rounded- shadow w-1/4 p-4">
+        <form className="flex flex-col" onSubmit={async (e) => {
         e.preventDefault();
 
         if (!file) return;
@@ -24,21 +28,30 @@ export default function HomePage() {
         const data = await res.json();
         console.log(data)
       }}>
-        <label>
-          Upload file:
-        </label>
+          <label className="text-center py-2 text-3xl font-semibold">
+            Upload file:
+          </label>
 
-        <input type="file"
-          onChange={(e) => {
-            setFile(e.target.files[0])
-          }}
-        />
+          <input className="text-center py-2 text-sm" type="file"
+            onChange={handleFileChange}
+          />
 
-        <button>
-          Upload
-        </button>
-
+          <button className="w-full bg-blue-500 text-white p-3 rounded-lg mt-2 hover:bg-blue-600 active:bg-blue-800 shadow transition-colors duration-200 font-bold">
+            Upload
+          </button>
       </form>
+
+        {file && (
+          <div className="flex justify-center items-center pt-4">
+            <Image
+            src={URL.createObjectURL(file)}
+            alt="Upload file"
+            width={300}
+            height={300}
+          />
+          </div>
+        )}
+      </section>
     </div>
   );
 };
